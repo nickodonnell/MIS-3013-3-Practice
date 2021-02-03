479898ceb984d55ed1cc74a3baa527b3dc95ca29
Nick O'Donnell <John.N.ODonnell-1@ou.edu>
2021-02-02 18:18:34 -0600

    Created a new solution for in class examples as well as for variables, inputs etc

---
+++ Variables Conversion Practice/Variables Conversion Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_Conversion_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
